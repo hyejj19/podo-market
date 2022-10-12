fdb8b81a687e0dddbae2bf4d001996c1564f0edb
hyejj19 <hyejj19@naver.com>
2022-10-12 20:35:50 +0900

    :sparkles: SWR 적용
    SWR 적용, 로그인 유저 데이터 fetch를 위한 useUser 훅 SWR로 리팩토링

--- src/lib/client/useUser.js
+++ src/lib/client/useUser.js
@@ -1,23 +1,23 @@
 import {useRouter} from 'next/router';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
+import useSWR from 'swr';
+
+const fetcher = url => fetch(url).then(res => res.json());
 
 export default function useUser() {
-  const router = useRouter();
-  const [user, setUser] = useState('');
-  useEffect(() => {
-    // /api/users/me 에 user 데이터 요청
-    fetch('/api/users/me')
-      .then(res => res.json())
-      .then(res => {
-        // 로그인되지 않은 경우 로그인 페이지로 리다이렉트
-        if (!res.ok) return router.replace('/enter');
-        setUser(res);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+  const router = useRouter('');
+  const {data, error} = useSWR('/api/users/me', fetcher);
 
-    // user data 에서 ok:false 를 리턴받은 경우 => router.replace 로 로그인 페이지로 이동한다.
-  }, [router]);
-  return user;
+  // 받아온 데이터가 false 라면 enter 페이지로 리다이렉트
+  useEffect(() => {
+    if (data && !data.ok) router.replace('/enter');
+  }, [data, router]);
+
+  return {user: data?.profile, isLoading: !data && !error};
 }
+
+/*
+isLoading
+- data와 error 둘 다 없으면 => 로딩중이므로 true
+- data와 error 둘 중 하나만 있으면 => 어쨌든 로딩이 되었으므로 false
+*/
